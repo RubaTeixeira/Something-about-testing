3ccad380c567a30dcbc60f75a5e08dd3e4b12cbc
RubaTeixeira <rubateixeira@hotmail.com>
2024-05-27 16:48:44 -0300

    New report settings, including SauceLabs build

--- 3_Mobile_Testing/wdio.conf.js
+++ 3_Mobile_Testing/wdio.conf.js
@@ -1,18 +1,41 @@
 export const config = {
-  runner: "local",
-  port: 4723,
+  //runner: "local",
+  //port: 4723,
+
+  //SauceLabs configuration
+  user: "oauth-rubateixeira-43f63",
+  key: "6e869825-20c3-46ce-ab1c-b0885ab7569f",
+  hostname: "ondemand.us-west-1.saucelabs.com",
+  port: 443,
+  baseUrl: "wd/hub",
+
   specs: ["./test/specs/**/*.js"],
   maxInstances: 1,
 
   capabilities: [
+    //{
+    //Capabilities for local Appium mobile tests on an Android Emulator
+    //platformName: "Android",
+    //"appium:deviceName": "MobTestDVC",
+    //"appium:platformVersion": "12.0",
+    //"appium:automationName": "UiAutomator2",
+    //"appium:app": `${process.cwd()}/app/ebacshop.apks`,
+    //"appium:appWaitActivity": ".MainActivity",
+    //"appium:disableIdLocatorAutocompletion": true,
+    //},
     {
-      // capabilities for local Appium web tests on an Android Emulator
+      //Capabilities for SauceLabs + Appium mobile to tests in a DeviceFarm
       platformName: "Android",
-      "appium:deviceName": "MobTestDVC",
+      "appium:app": "storage:filename=ebacshop.aab", // The filename of the mobile app
+      "appium:deviceName": "Android GoogleAPI Emulator",
       "appium:platformVersion": "12.0",
       "appium:automationName": "UiAutomator2",
-      "appium:app": `${process.cwd()}/app/ebacshop.apks`,
-      "appium:appWaitActivity": ".MainActivity",
+      "sauce:options": {
+        appiumVersion: "2.0.0",
+        build: "appium-build-J0GIU",
+        name: "Mobile Test Portfolio",
+        deviceOrientation: "PORTRAIT",
+      },
       "appium:disableIdLocatorAutocompletion": true,
     },
   ],
@@ -21,7 +44,17 @@ export const config = {
   connectionRetryTimeout: 120000,
   connectionRetryCount: 3,
   framework: "mocha",
-  reporters: ["spec"],
+  reporters: [
+    "spec",
+    [
+      "allure",
+      {
+        outputDir: "allure-results",
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: true,
+      },
+    ],
+  ],
   mochaOpts: {
     ui: "bdd",
     timeout: 60000,
